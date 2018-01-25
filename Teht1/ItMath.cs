b7fd14fbc14802c77dc29635f68e5fcc64e9aac2
Antti Virkkunen <antti.virkkunen@gmail.com>
2018-01-25 17:33:29 +0200

    Edits to T1, also the solution file

---
+++ Teht1/ItMath.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Teht1
+{
+    static class ItMath
+    {
+        /// <summary>
+        /// Is a given text string a number?
+        /// Uses same criteria as float.TryParse, except that a digit string
+        /// beginning or ending with the system decimal separator is not
+        /// considered a number.
+        /// </summary>
+        /// <param name="text">Text string to examine</param>
+        /// <returns>True if the string is a number, False if it is not</returns>
+        public static bool IsNumber(String text)
+        {
+            char decimalSeparator = Convert.ToChar(System.Threading.Thread.CurrentThread.CurrentCulture.NumberFormat.NumberDecimalSeparator);   //Aika vonkale
+            if (text[0] == decimalSeparator || text[text.Length - 1] == decimalSeparator) { return false; }
+            return float.TryParse(text, out float drop);
+        }
+    }
+}
--- Teht1/TestBed.cs
+++ Teht1/TestBed.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Teht1
 {
@@ -16,21 +12,16 @@ namespace Teht1
             Console.WriteLine("-- Testing bad strings --");
             foreach (String test in testStringsBad)
             {
-                Console.WriteLine("String \"{0}\" is a number: {1}", test, IsNumber(test));
+                Console.WriteLine("String \"{0}\" is a number: {1}", test, ItMath.IsNumber(test));
             }
 
             Console.WriteLine("-- Testing good strings --");
             foreach (String test in testStringsGood)
             {
-                Console.WriteLine("String \"{0}\" is a number: {1}", test, IsNumber(test));
+                Console.WriteLine("String \"{0}\" is a number: {1}", test, ItMath.IsNumber(test));
             }
         }
 
-        public static bool IsNumber(String text)
-        {
-            char decimalSeparator = Convert.ToChar(System.Threading.Thread.CurrentThread.CurrentCulture.NumberFormat.NumberDecimalSeparator);   //Aika vonkale
-            if (text[0] == decimalSeparator || text[text.Length - 1] == decimalSeparator) { return false; }
-            return float.TryParse(text, out float drop);
-        }
+        
     }
 }
