11218b0d87d51c7d0132c46246dcf312a8a400cd
Antti Virkkunen <antti.virkkunen@gmail.com>
2018-01-25 18:24:39 +0200

    T2

--- Teht1/ItMath.cs
+++ Teht1/ItMath.cs
@@ -18,5 +18,19 @@ namespace Teht1
             if (text[0] == decimalSeparator || text[text.Length - 1] == decimalSeparator) { return false; }
             return float.TryParse(text, out float drop);
         }
+
+        /// <summary>
+        /// Is a given text string a date?
+        /// Accepted date formats are
+        /// "dd.MM.yyyy", "dd.MM.yy", "d.M.yyyy" and "d.M.yy"
+        /// </summary>
+        /// <param name="text">Text string to examine</param>
+        /// <returns>True if the string is a date, False if it is not</returns>
+        public static bool IsDate(string text)
+        {
+            string[] formats = { "dd.MM.yyyy", "dd.MM.yy", "d.M.yyyy", "d.M.yy" };
+            return DateTime.TryParseExact(text, formats, System.Globalization.CultureInfo.InvariantCulture, System.Globalization.DateTimeStyles.None, out DateTime drop);
+            // I don't know what half these parameters do but it seems to work
+        }
     }
 }
--- Teht1/TestBed.cs
+++ Teht1/TestBed.cs
@@ -9,6 +9,18 @@ namespace Teht1
             String[] testStringsBad = { "Kissa", "abcABC1", "1234.", ",1234", "1234,", "12-34", "123.45", "123,45,67" };
             String[] testStringsGood = { "0", "123", "123,45", "+123", "-123", "+123,45", "-123,45" };
 
+            RunNumberTest(testStringsBad, testStringsGood);
+
+            Console.WriteLine();
+
+            String[] dateStringsBad = { "12,12,2016", "12.13.2017", "30.2.2017" };
+            String[] dateStringsGood = { "1.2.13", "1.10.2017", "12.12.2017", "24.12.17" };
+
+            RunDateTest(dateStringsBad, dateStringsGood);
+        }
+
+        private static void RunNumberTest(string[] testStringsBad, string[] testStringsGood)
+        {
             Console.WriteLine("-- Testing bad strings --");
             foreach (String test in testStringsBad)
             {
@@ -22,6 +34,20 @@ namespace Teht1
             }
         }
 
-        
+        private static void RunDateTest(string[] testStringsBad, string[] testStringsGood)
+        {
+            Console.WriteLine("-- Testing bad strings --");
+            foreach (String test in testStringsBad)
+            {
+                Console.WriteLine("String \"{0}\" is a date: {1}", test, ItMath.IsDate(test));
+            }
+
+            Console.WriteLine("-- Testing good strings --");
+            foreach (String test in testStringsGood)
+            {
+                Console.WriteLine("String \"{0}\" is a date: {1}", test, ItMath.IsDate(test));
+            }
+        }
+
     }
 }
